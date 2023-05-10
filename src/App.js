c8f9646172b86089b52629375bb4a4a418bd7f41
Thein Naing <theinnaing0908@gmail.com>
2023-05-10 20:25:29 +0800

    create List.js/ update App.js / and render List.js component in App.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
+
+function App() {
+  return
+  <main>
+    <section class="container">
+      <h3>0 birthdays today</h3>
+      < List />
+
+    </section>
+  </main>
+}
+
+export default App;

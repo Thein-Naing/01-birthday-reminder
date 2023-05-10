ab8ed4cefb2a8c75c9286af665ca93123e9d28ef
Thein Naing <theinnaing0908@gmail.com>
2023-05-10 20:30:38 +0800

    create useState hook for people with data initial state to render people data

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import data from './data';
 import List from './List';
 
 function App() {
-  return
+  const[people, setPeople] = useState(data);
+  return (
   <main>
     <section class="container">
       <h3>0 birthdays today</h3>
       < List />
-      <button onClick={() => console.log("you click me!")}></button>
+      <button onClick={() => console.log("you click me!")}>
       Clear All</button>
-
     </section>
   </main>
-}
+    );
+  }
 
 export default App;

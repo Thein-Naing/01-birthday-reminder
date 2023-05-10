ac13446c725e4eb2bb12eff131f45b825db6c219
Thein Naing <theinnaing0908@gmail.com>
2023-05-10 20:57:06 +0800

    return person keys with html tags and {}

--- src/List.js
+++ src/List.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
-const List = (people) => {
+const List = ({people}) => {
   return (
     <>
     {people.map((person)=>{
       const {id, name, age, image} = person;
+      return (
+      <article key={id} className="person">
+        <image src={image} alt={name}/>
+        <div>
+          <h4>{name}</h4>
+          <p>{age}years</p>
+        </div>
+      </article>
+    );
     })}
     </>
   );
